2ad86fbb3c25e0b88bb3d7e0faefecc647a9db5c
Zameel Abdul Sammed <zameelabdulsamad@gmail.com>
2023-07-08 19:35:33 +0530

    feat: module-print

--- power.py
+++ power.py
@@ -1,35 +1,9 @@
-import subprocess
-import time
-import serial
+import uhubctl
 
-def power_off_usb_device(device_path):
-    subprocess.run(['sudo', 'usbreset', device_path])
+hubs = uhubctl.discover_hubs()
 
-def power_on_usb_device(device_path):
-    subprocess.run(['sudo', 'usbreset', device_path])
+for hub in hubs:
+    print(f"Found hub: {hub}")
 
-def read_qr_code():
-    try:
-        # Open serial connection
-        ser = serial.Serial("/dev/ttyACM0", 115200, timeout=0.1)
-        print("Connected to the QR code scanner.")
-    except serial.SerialException:
-        print("Failed to connect to the QR code scanner.")
-        sys.exit(1)
-
-    # Determine the USB device path for /dev/ttyACM0
-    device_path = ser.port
-
-    power_off_usb_device(device_path)
-    time.sleep(10)  # Sleep for 10 seconds
-
-    power_on_usb_device(device_path)
-
-    while True:
-        line = ser.readline().decode().strip()
-        if line:
-            print("QR code:", line)
-
-    ser.close()
-
-read_qr_code()
+    for port in hub.ports:
+        print(f"   Found port: {port}")
